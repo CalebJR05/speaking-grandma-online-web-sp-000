4147e69352bca8d2f7335e233c4f0c9304b15c39
Caleb R <cjr4912@gmail.com>
2020-07-17 16:54:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,16 +1,16 @@
 def speak_to_grandma(phrase)
   
   if 
-    phrase == String.downcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
     
   elsif 
-    phrase == String.upcase
-    puts "NO, NOT SINCE 1938!"
+    phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
     
   else 
     phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
     
   end
 end
