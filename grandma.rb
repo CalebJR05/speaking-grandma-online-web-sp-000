999775a358b01cc348e005df7492a7f66fee7965
Caleb R <cjr4912@gmail.com>
2020-07-17 16:57:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
     phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
     
-  else 
+  elsif 
     phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
