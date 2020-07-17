a2b1460084271b18298fc5669b53d1e74f4f8b47
Caleb R <cjr4912@gmail.com>
2020-07-17 17:05:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,13 +4,13 @@ def speak_to_grandma(phrase)
     phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
     
-  elsif 
-    phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-    
-  else 
+  elsif
     phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+    
+  else 
+    phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"  
     
   end
 end
